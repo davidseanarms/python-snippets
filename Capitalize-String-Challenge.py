3b18040c639c0566eec923f5981f5c1bce254462
David Seán <85075382+davidseanarms@users.noreply.github.com>
2022-12-13 16:19:29 +0100

    Update Capitalize-String-Challenge.py

--- Capitalize-String-Challenge.py
+++ Capitalize-String-Challenge.py
@@ -1,19 +1,4 @@
 def capitalize():
-    str = input("Enter a string to capitalize: ")
-    words = str.split(' ')
-    words_capitalized = []
-    for word in words:
-        first_letter = word[0].upper()
-        rest_of_word = word[1:]
-        capitalized_word = first_letter + rest_of_word
-        words_capitalized.append(capitalized_word)
-    return ' '.join(words_capitalized)
-def main():
-    capitalized_string = capitalize()
-    print(capitalized_string)
-if __name__ == '__main__':
-    main()
-def capitalize():
     user_string = input("Enter a string to capitalize: ")
     words = user_string.split(' ')
     capitalized_words = []
