0c85902ff39297712404b544d4c00b1567390680
David Seán <85075382+davidseanarms@users.noreply.github.com>
2022-12-22 14:45:33 +0100

    Create 4-for-price-3-calculator.py

---
+++ 4-for-price-3-calculator.py
@@ -0,0 +1,16 @@
+def FourForThreeDiscount(p1, p2, p3, p4):
+    cheapest_product = min(p1, p2, p3, p4)
+    total_cost = p1 + p2 + p3 + p4
+    discounted_cost = total_cost - cheapest_product
+    discount_percent = (discounted_cost / total_cost) * 100
+    discount_amount = total_cost - discounted_cost
+    avg_cost_per_product = discounted_cost / 3
+    print("What you pay: ", discounted_cost)
+    print("One item costs on average: ", avg_cost_per_product)
+    print("Your saving in $ form: ", discount_amount)
+
+p1 = float(input("Enter the price of the first product: "))
+p2 = float(input("Enter the price of the second product: "))
+p3 = float(input("Enter the price of the third product: "))
+p4 = float(input("Enter the price of the fourth product: "))
+FourForThreeDiscount(p1, p2, p3, p4)
