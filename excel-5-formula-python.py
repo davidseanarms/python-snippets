3d3a805cf753f46ae9c0e842e8563eae7271959b
David Seán <85075382+davidseanarms@users.noreply.github.com>
2022-12-29 11:19:40 +0100

    Create excel-5-formula-python.py

---
+++ excel-5-formula-python.py
@@ -0,0 +1,30 @@
+# 5 common Excel formulas expressed as Python
+# Notes, snippets.
+
+# 1. SUM: 
+sum = 0
+for x in range(len(x)):
+    sum = sum + x[i]
+    
+# 2. AVERAGE: 
+average = 0
+for x in range(len(x)):
+    average += x[i]
+average = average/len(x)
+
+# 3. COUNT: 
+count = 0
+for x in range(len(x)):
+    count += 1
+    
+# 4. MAX:
+max = x[0]
+for i in range(1, len(x)):
+    if x[i] > max:
+        max = x[i]
+
+# 5. MIN: 
+min = x[0]
+for i in range(1, len(x)):
+    if x[i] < min:
+        min = x[i]
